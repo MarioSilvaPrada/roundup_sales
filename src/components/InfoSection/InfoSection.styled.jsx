c8a01cabe095a5bbca0a22ea6c9677a24e5fc34d
Mário Prada <mariosilvaprada@gmail.com>
2020-02-20 13:41:35 +0000

    updating

--- src/components/InfoSection/InfoSection.styled.jsx
+++ src/components/InfoSection/InfoSection.styled.jsx
@@ -12,12 +12,17 @@ export const Wrapper = styled.div`
   margin: 0 auto;
   margin-bottom: 2.5rem;
 
-  &  > *:not(:first-child) {
+  & > *:not(:first-child) {
     margin-top: 10rem;
   }
 `;
 
-export const StyledImg = styled.img`width: 30rem;`;
+export const StyledImg = styled.img`
+  width: 30rem;
+  @media (max-width: 740px) {
+    width: 20rem;
+  }
+`;
 export const Content = styled.div`
   display: flex;
   ${(props) => props.reverse && 'flex-direction: row-reverse'};
@@ -26,14 +31,36 @@ export const Content = styled.div`
   box-shadow: ${BOX_SHADOW};
   padding: 4rem 5rem;
   border-radius: 2rem;
+  transform: translate(${(props) => (props.reverse ? '20%' : '-20%')});
+  min-height: 35rem;
+
+  @media (max-width: 1600px) {
+    transform: translate(0);
+  }
   @media (max-width: 940px) {
     flex-direction: column-reverse;
+  }
+
+  @media (max-width: 740px) {
+    padding: 2rem;
   }
 `;
 
 export const RichText = styled.div`
   display: flex;
   flex-direction: column;
+  line-height: 1.3rem;
+
+  h1,
+  h2 {
+    line-height: 2.8rem;
+  }
+
+  h3,
+  h4,
+  h5 {
+    line-height: 2rem;
+  }
 
   li {
     list-style-type: circle;
@@ -45,4 +72,4 @@ export const StyledTitle = styled.h1`
   margin: 5rem 0;
   font-size: 2.8rem;
   color: ${BLUE};
-`
+`;
